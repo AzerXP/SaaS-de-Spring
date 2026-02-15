3bfa292c8235932c88141b7ffc24080bac5e55a4
DaryllLorenzo <vikingwarrior4444@gmail.com>
2026-02-15 07:46:39 -0500

    feat: Added initial test and impl files

--- src/main/java/com/saas/spring/achievement/AchievementController.java
+++ src/main/java/com/saas/spring/achievement/AchievementController.java
@@ -1,4 +1,15 @@
 package com.saas.spring.achievement;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/achievements")
 public class AchievementController {
+
+    @GetMapping
+    public String getAchievements(){
+        return "as";
+    }
 }
--- src/test/java/com/saas/spring/achievement/AchievementControllerTest.java
+++ src/test/java/com/saas/spring/achievement/AchievementControllerTest.java
@@ -1,4 +1,21 @@
 package com.saas.spring.achievement;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AchievementController.class)
 public class AchievementControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void itWorks() throws Exception {
+        mockMvc.perform(get("/achievements"))
+                .andExpect(status().isOk());
+    }
 }
