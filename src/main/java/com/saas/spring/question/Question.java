a6b4fa912c8b12445484784d27866a5823c9553d
DaryllLorenzo <vikingwarrior4444@gmail.com>
2026-02-28 10:24:12 -0500

    Cambio de atributo user_id a nullable para poder probar creaciones

--- src/main/java/com/saas/spring/question/Question.java
+++ src/main/java/com/saas/spring/question/Question.java
@@ -29,7 +29,7 @@ public class Question {
     private QuestionType questionType;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = true)
     private User user;
 
     @OneToOne(
